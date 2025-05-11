260a9d06612ae977e81e494d795ad50049a4ec00
Vasily Satantsev <vasily.satantsev@gmail.com>
2025-05-11 19:57:54 +0300

    HW#9 modifyed

--- HW#9/commands.py
+++ HW#9/commands.py
@@ -1,4 +1,21 @@
 sql=SQLContext(sc)
 import pyspark.sql.functions as F
 
-df = spark.read.option("multiline", "true").json("s3a://etl-storages/json_data.json", miltiLine=True)
+
+
+# Пути к файлам
+json_path = ""  
+parquet_sessions_path = ""
+parquet_actions_path = ""
+
+# Чтение JSON
+raw_df = spark.read.option("multiline", "true").json(json_path)
+
+# Извлечение и развертывание массивов
+sessions_df = raw_df.select(F.explode("sessions").alias("session")).select("session.*")
+actions_df = raw_df.select(F.explode("actions").alias("action")).select("action.*")
+
+# Сохранение в формате Parquet
+sessions_df.write.mode("overwrite").parquet(parquet_sessions_path)
+actions_df.write.mode("overwrite").parquet(parquet_actions_path)
+
